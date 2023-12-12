6bd552d5230a08b04ba31103f9dbfaf3019b6757
arifpirxada <pirxadaarif@gmail.com>
2023-12-13 00:09:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Forums from "./components/Forums";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Forums />
+    </>
+  );
+}
+
+export default App;
